43cc431bb9a1a42c971efd8ba790ba00a9357337
cellsium <52256266+cellsium@users.noreply.github.com>
2020-12-09 08:35:15 +0300

    t

---
+++ waxom/script/app.js
@@ -0,0 +1,160 @@
+const slider = document.querySelector('.header__slider');
+const sliderLine = document.querySelector('.header__slider-line');
+const sliderBlock = document.querySelectorAll('.header__slider-block');
+
+let position;
+let count;
+let maxBlock;
+let blockPosition = 0;
+let maxWidth = 0;
+let speedSlide = 0.6;
+
+window.addEventListener('resize', size);
+
+size();
+
+function size() {
+    sliderBlock.forEach(elem => {
+        elem.style.width = window.innerWidth + 'px';
+        maxWidth += parseFloat(elem.style.width);
+    });
+}
+
+function upMouse() {
+    if (count < 0 && blockPosition != -(maxWidth - window.innerWidth)) {
+        blockPosition -= window.innerWidth;
+    } else if (count > 0 && blockPosition != 0) {
+        blockPosition += window.innerWidth;
+    }
+    removeEventListener('mousemove', moveMouse);
+    removeEventListener('mousemove', downMouse);
+    position = 0;
+    count = 0;
+}
+
+function downMouse(event) {
+    count = 0;
+    position = 0;
+    position = event.pageX;
+    slider.addEventListener('mousemove', moveMouse);
+}
+
+function moveMouse(event) {
+    if (position < event.pageX && position != 0) {
+        count = count + 1;
+    } else if (position > event.pageX && position != 0) {
+        count = count - 1;
+    }
+    sliderLine.style.transition = speedSlide + 's';
+    sliderLine.style.transform = `translate(${blockPosition}px)`;
+}
+
+function left() {
+    if (blockPosition != -(maxWidth - innerWidth)) {
+        blockPosition -= window.innerWidth;
+    }
+    sliderLine.style.transform = `translate(${blockPosition}px)`;
+}
+
+function right() {
+    if (blockPosition != 0) {
+        blockPosition += window.innerWidth;
+    }
+    sliderLine.style.transform = `translate(${blockPosition}px)`;
+}
+
+slider.addEventListener('mousedown', downMouse);
+slider.addEventListener('mouseup', upMouse);
+document.querySelector('.left').addEventListener('click', left);
+document.querySelector('.right').addEventListener('click', right);
+
+
+
+
+// nextSlider
+
+//  chief                         Контейнер слайдера
+//  sliderLine                    контейнер с блоками слайдера
+//  sliderBlock                   блок слайдера
+//  arrowLeft                     кнопка влево
+//  arrowRight                    кнопка вправо
+//  marginBlock                   если есть margin слева и справа от блок слайдера
+//  transition                    transition для плавной прокрутки
+//  countSlideBlock               количество перелистывания блоков
+//  lineWidthBlock                сколько блоков показывать в слайдере
+
+
+function sliderMove(chief, sliderLine, sliderBlock, arrowLeft, arrowRight, marginBlock, transition, countSlideBlock, lineWidthBlock) {
+
+    const chiefSlider = document.querySelector('.' + chief);
+    const line = document.querySelector('.' + sliderLine);
+    const block = document.querySelectorAll('.' + sliderBlock);
+    const left = document.querySelector('.' + arrowLeft);
+    const right = document.querySelector('.' + arrowRight);
+
+    let sizeBlock;
+    let maxSize = 0;
+    let count = 0;
+
+
+
+    block.forEach(item => {
+        sizeBlock = item.offsetWidth;
+        maxSize += item.offsetWidth + marginBlock * 2;
+    })
+
+    function sizeLine() {
+        chiefSlider.style.width = (sizeBlock + (marginBlock * 2)) * lineWidthBlock + 'px';
+    }
+
+    if (lineWidthBlock == 1) {
+        countSlideBlock = 1;
+    }
+
+
+    function leftSlide() {
+        if (count < maxSize / 2) {
+            count += (sizeBlock + marginBlock * 2) * countSlideBlock;
+        }
+        line.style.transition = transition + 's';
+        line.style.transform = `translate(-${count}px)`;
+        console.log(maxSize / 2);
+
+    }
+
+    function rightSlide() {
+        if (count != 0) {
+            count -= (sizeBlock + marginBlock * 2) * countSlideBlock;
+        }
+
+        line.style.transition = transition + 's';
+        line.style.transform = `translate(-${count}px)`;
+    }
+
+    // управление мышкой
+
+    let touch = 0;
+    let up = 0;
+
+    function touchDown(e) {
+        touch = e.clientX;
+    }
+
+    function touchUp(e) {
+        up = e.clientX;
+        if (up < touch) {
+            leftSlide();
+        } else {
+            rightSlide();
+        }
+    }
+
+    sizeLine();
+
+    line.addEventListener('mousedown', touchDown);
+    line.addEventListener('mouseup', touchUp);
+    left.addEventListener('click', leftSlide);
+    right.addEventListener('click', rightSlide);
+}
+
+sliderMove('posts__slider', 'posts__slider-line', 'posts__slider-block', 'arrow-left', 'arrow-right', 10, 0.3, 1, 3);
