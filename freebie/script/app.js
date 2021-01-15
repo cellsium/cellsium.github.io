c0b320bdc05a40dac1012b38bddfbc7aa05a72cf
cellsium <52256266+cellsium@users.noreply.github.com>
2021-01-15 21:08:26 +0300

    freebie fin

--- freebie/script/app.js
+++ freebie/script/app.js
@@ -1,12 +1,54 @@
-const link = document.querySelectorAll('.nav');
+function toggleClass(targetItem, primaryClass, secondaryClass, outNumber, adaptiv) {
 
-link.forEach(item => {
-  item.addEventListener('click', () => {
-    link.forEach(item => {
-      item.classList.remove('active');
-      item.classList.add('deactive');
+  // targetItem       класс элементов который нужно листать
+  // primaryClass     название класса который делает элемент активным
+  // secondaryClass   название класса который делает элемент не активным
+  // outNumber        класс блока в который нудно выводить кликабельную нумерацию элементов ('' -- отключено)
+  // adaptiv          изменение ширины блока равную ширине окна браузера ('' -- отключено)
+
+  const link = document.querySelectorAll('.' + targetItem);
+
+  link.forEach((item, value) => {
+
+    if (adaptiv != '') {
+      item.style.width = innerWidth + 'px';
+      // item.style.height = innerHeight + 'px';
+    }
+
+    if (outNumber != '') {
+      let p = document.createElement('p');
+      p.classList.add('p');
+      p.innerHTML = '0' + (value + 1);
+      document.querySelector('.' + outNumber).append(p);
+
+      p.addEventListener('click', () => {
+        document.querySelectorAll('.p').forEach(item => {
+          item.style.opacity = '.4';
+          item.style.transform = 'scale(1)';
+        });
+        p.style.transform = 'scale(2)';
+        p.style.opacity = '1';
+        link.forEach(item => {
+          item.classList.remove('visible');
+          item.classList.add('unvisible');
+        })
+        if (item.classList != 'visible') {
+          item.classList.remove('unvisible');
+          item.classList.add('visible');
+        }
+      })
+    };
+
+    item.addEventListener('click', () => {
+      link.forEach(item => {
+        item.classList.remove(primaryClass);
+        item.classList.add(secondaryClass);
+      });
+      item.classList.remove(secondaryClass);
+      item.classList.add(primaryClass);
     });
-    item.classList.remove('deactive');
-    item.classList.add('active');
   });
-});
+}
+
+toggleClass('nav', 'active', 'deactive', '', '');
+toggleClass('header__container-block', 'visible', 'unvisible', 'header-point');
