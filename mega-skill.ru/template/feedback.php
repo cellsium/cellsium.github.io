3edb6a63987231f570a027e3b80bbf19069c31bc
cellsium <52256266+cellsium@users.noreply.github.com>
2021-10-25 23:17:44 +0300

    mega-skill_1

--- mega-skill.ru/template/feedback.php
+++ mega-skill.ru/template/feedback.php
@@ -2,14 +2,14 @@
 <form id="form">
       <div class="personality">
         <input id="name" type="text" name="name" required placeholder="Имя">
-        <input id="middlename" type="text" name="middlename" required placeholder="Отчество">
         <input id="family" type="text" name="family" required placeholder="Фамилия">
+        <input id="middlename" type="text" name="middlename" required placeholder="Отчество">
         <input id="phone" type="number" name="phone" required placeholder="Номер телефона (88888888888)">
-        <input id="sity" type="text" name="sity" required placeholder="Город проживания">
-        <input id="skill" type="text" name="skill" required placeholder="опыт работы">
+        <input id="sity" type="text" name="sity" required placeholder="Ваш город (для поиска работы в вашем городе)">
+        <input id="skill" type="text" name="skill" required placeholder="опыт работы (сколько лет в судостроении)">
         <input id="trip" type="text" name="trip" required placeholder="Готовность к командировкам (готов/не готов)">
         <input id="team" type="text" name="team" required placeholder="Опыт работы в коллективе (есть/нету)">
-        <input id="group" type="text" name="group" required placeholder="Своя бригада людей (колличество/нету)">
+        <input id="group" type="text" name="group" required placeholder="Своя бригада (колличество людей/нету)">
         <input id="invite" type="text" name="invite" placeholder="Кто пригласил">
         <textarea id="work" type="text" name="work" required placeholder="Где работал" maxlength="1000"></textarea>
         <p>Нажимая на кнопку, вы даете согласие на обработку своих персональных данных <a href="/document.pdf"
--- mega-skill.ru/template/main.php
+++ mega-skill.ru/template/main.php
@@ -5,13 +5,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Минимальная ставка 500 рублей в час,у нас вы найдете высокооплачиваемую работу">
+    <meta name="yandex" content="all" />
+    <meta name="google" content="all" />
+    <meta name="slurp" content="all" />
+    <meta name="bingbot" content="all" />
     <link rel="shortcut icon" href="./static/images/favico.png">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Fira+Mono:wght@400;500;700&family=PT+Sans&display=swap"
         rel="stylesheet">
     <link rel="stylesheet" href="./static/style/style.css">
-    <title>Mega-Skill</title>
+    <title>Поиск квалифицированных сотрудников в области судостроения</title>
 </head>
 
 <body>
